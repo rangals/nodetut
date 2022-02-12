6a0341b92080ba34a6e4347b54686f7f80c640e2
rangals <rangalsoftwares@gmail.com>
2022-02-12 23:45:21 +0530

    Post and db connect

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
+const mysqlx = require('@mysql/xdevapi');
+const conf = require('./config');
+
 const app = express();
 app.use(express.json());
+const dbDetails = conf["DB"];
 
 const books = [
 {title: 'Harry Potter', id: 1},
 {title: 'Twilight', id: 2},
 {title: 'Lorien Legacies', id: 3}
 ]
+
+const config = {
+    password: dbDetails.password,
+    user: dbDetails.user,
+    host: dbDetails.host,
+    port: dbDetails.port,
+    schema: dbDetails.schema
+    
+};
  
 //READ Request Handlers
 app.get('/', (req, res) => {
@@ -26,6 +39,48 @@ const book = books.find(c => c.id === parseInt(req.params.id));
 if (!book) res.status(404).send('<h2 style="font-family: Malgun Gothic; color: darkred;">Ooops... Cant find what you are looking for!</h2>');
 res.send(book);
 });
+
+//{"title": "new byox"}
+//http://localhost:8080/api/books
+app.post('/api/books', (req, res)=> {
+ 
+    const book = {
+        id: books.length + 1,
+        title: req.body.title
+    };
+    books.push(book);
+    res.send(book);
+    });
+
+    mysqlx.getSession(config)
+    .then(session => {
+        // console.log(session.inspect()); // { user: 'root', host: 'localhost', port: 33060 }
+        const table = session.getSchema(config.schema).getTable('tbluser');
+        console.log("connected to table");
+        return table.select()
+            .where('usrName = "Barani1"')
+            .execute();
+    })
+    .then((result) => {
+        console.log(result.fetchAll());//To fetch all data. fetchOne() to fetch one record at a time
+    });
+
+    app.get('/api/login', (req,res)=> {
+        mysqlx.getSession(config)
+    .then(session => {
+        // console.log(session.inspect()); // { user: 'root', host: 'localhost', port: 33060 }
+        const table = session.getSchema(config.schema).getTable('tbluser');
+        console.log("connected to table");
+        return table.select()
+            .where('usrName = "Barani1"')
+            .execute();
+    })
+    .then((result) => {
+        res.send(result.fetchAll());//To fetch all data. fetchOne() to fetch one record at a time
+    });
+        
+        });
+//{"uname": "Barani1", "pwd":"hai" }        
 
 //PORT ENVIRONMENT VARIABLE
 const port = process.env.PORT || 90;
